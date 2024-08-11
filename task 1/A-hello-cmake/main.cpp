407889e2f4593ae60f1c678da2ae91bba053ee0b
ZlhDragon <1466599673@qq.com>
2024-08-11 01:32:59 -0400

    Add files via upload

---
+++ task 1/A-hello-cmake/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // 引入输入输出流库
+
+int main(int argc, char *argv[])
+{
+   std::cout << "Hello CMake!" << std::endl; // 输出 "Hello CMake!" 并换行
+   return 0; // 返回 0 表示程序正常结束
+}
---
+++ task 1/C-static-library/src/main.cpp
@@ -0,0 +1,13 @@
+#include "static/Hello.h"  // 包含 Hello 类的声明头文件，路径为 "static/Hello.h"
+
+int main(int argc, char *argv[])
+{
+    // 创建 Hello 类的一个对象 hi
+    Hello hi;
+
+    // 调用 hi 对象的 print 方法，打印消息 "Hello Static Library!"
+    hi.print();
+
+    // 返回 0 表示程序成功结束
+    return 0;
+}
