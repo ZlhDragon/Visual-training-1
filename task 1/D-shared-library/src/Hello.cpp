94eb149e49c5109bc3b480c0baa9aaf9e79f434c
ZlhDragon <1466599673@qq.com>
2024-08-12 01:51:06 -0400

    Delete task 1 directory

--- task 1/D-shared-library/src/Hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>  // 包含标准输入输出流库，用于输出信息
-
-#include "shared/Hello.h"  // 包含 Hello 类的声明头文件，路径为 "shared/Hello.h"
-
-void Hello::print()
-{
-    // 使用 std::cout 输出 "Hello Shared Library!" 字符串，并换行
-    std::cout << "Hello Shared Library!" << std::endl;
-}
